6d2153209bdb63eb4a071ecb303e44f587d484bd
mitessier <mitessier@maisonsdumonde.com>
2023-10-25 06:29:54 +0200

    feat: add stylelint

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="flex flex-col items-center justify-center h-screen">
-    <HelloWorld title="Vue hyper" text="Hello world" />
+    <HelloWorld
+      title="Vue hyper"
+      text="Hello world"
+      color="green"
+    />
   </div>
 </template>
