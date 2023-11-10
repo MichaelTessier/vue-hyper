ca32cbe7b7eb6d101acb709fa2c18ec99872c4d2
mitessier <mitessier@maisonsdumonde.com>
2023-11-10 18:26:12 +0100

    feat: add workspaces

---
+++ packages/admin/uno.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+  theme: {
+    colors: {
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+      white: 'var(--color-white)',
+      light: 'var(--color-light)',
+      dark: 'var(--color-dark)',
+      info: 'var(--color-info)',
+      success: 'var(--color-success)',
+      warning: 'var(--color-warning)',
+      danger: 'var(--color-danger)',
+    },
+  },
+})
